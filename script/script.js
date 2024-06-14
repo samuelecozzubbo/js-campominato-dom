95cb626d978edbd0c0377208a0e4dbbe1e82abbe
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-14 16:24:05 +0200

    Build: create addClassBomOrSafe function

--- script/script.js
+++ script/script.js
@@ -5,6 +5,10 @@ let alreadyPlayed = false;
 
 // L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 myBtn.addEventListener("click",generateGrid);
+//creo l'array di numeri in ordine casuale
+const gridNumbers = genSequenzaNumRandom(1,100,10);
+console.log(gridNumbers);
+
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
@@ -23,11 +27,16 @@ function generateGrid() {
         //aggiungo il blocco al contenitore
         container.append(nuovobox);
         nuovobox.append(i + 1);
-        }   
-        //Invoco funzione add class
-        addClass();
-        //cambio stato variabile
-        alreadyPlayed = true;
+        }
+
+    //Invoco funzione add class
+    addClassWithLinstener("clicked");
+    //
+    addClassBombOrSafe();
+    //cambio stato variabile
+    alreadyPlayed = true;
+    
+    
     }     
 }
 
@@ -42,17 +51,67 @@ function createElementWithClass(tag, classToAdd) {
 }
 
 
-//Funzione add class 
-function addClass() {
+//Funzione add class with linstener
+function addClassWithLinstener(className) {
     //Richiamo i box
     const box = document.querySelectorAll(".square");
     for(let i = 0; i < 100; i++) {
     //Gestisco il click sul singolo elemento
     box[i].addEventListener("click",
         function(){
-            this.classList.toggle("clicked");
+            this.classList.toggle(className);
             console.log("Hai cliccato il box",i + 1);
         }
     );
     }   
+}
+
+
+
+//Funzione add class
+function addClassBombOrSafe() {
+    //Richiamo i box
+    const box = document.querySelectorAll(".square");
+    console.log("Sto funzionando");
+    for(let i = 0; i < 100; i++) {
+        //aggiungo classi bomb o safe
+        let numeroIesimo = gridNumbers[i];
+        console.log(numeroIesimo);
+        if(numeroIesimo === i){
+            box[i].classList.add =" bomb";
+        } else{
+            box[i].classList.add = "safe";
+        }
+    }   
+}
+
+
+//funzione che genera un numero random 
+    //Funzione
+    function RandomNumberGenerator(min, max) {
+        let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        return randomNumber; 
+    }
+
+
+//Funzione che genera un array di numeri in un range in ordine casuale
+
+function genSequenzaNumRandom(min, max, numberToGen) {
+    const arrToGen = [];
+
+    //Fino a quando arrToGen.lenght = numero di elementi voluto
+
+    
+    while(arrToGen.length < numberToGen){
+        //genero numero random nel range stabilito
+        let newRandomNum = RandomNumberGenerator(min,max);
+
+        //vedo se inserire o meno nell array(se non già compreso)
+        if(!arrToGen.includes(newRandomNum)){ //se arrtogen non inlcude newrandom num
+            arrToGen.push(newRandomNum);
+
+        }
+    }
+    return arrToGen
+
 }
